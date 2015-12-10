f5d1e004306782fec3431dded624f867c34768dd
gmlp <gmlp>
2015-12-10 18:23:19 +0000

    [REF] Add partner view and model inherit

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -20,7 +20,8 @@
     'data': [
         # 'security/ir.model.access.csv',
         'view/openacademy_course_view.xml',
-        'view/openacademy_session_view.xml'
+        'view/openacademy_session_view.xml',
+	'view/partner_view.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
