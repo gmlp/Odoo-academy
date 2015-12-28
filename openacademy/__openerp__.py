dffb392e3239da91af5dba8daeb9396a7e700267
gmlp <glopez@vauxoo.com>
2015-12-28 06:05:39 +0000

    [REF] openacademy: Adding info to the report

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -25,7 +25,8 @@
         'workflow/openacademy_session_workflow.xml',
         'security/openacademy_security.xml',
         'security/ir.model.access.csv',
-        'view/openacademy_wizard_view.xml'
+        'view/openacademy_wizard_view.xml',
+	'report/openacademy_session_report.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
