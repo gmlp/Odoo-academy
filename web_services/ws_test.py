eec0ef430124ec915963c80c6f55ee53f5ff07f4
gmlp <glopez@vauxoo.com>
2015-12-28 15:44:34 +0000

    [REF] ws: Adding m2m value

--- web_services/ws_test.py
+++ web_services/ws_test.py
@@ -29,17 +29,22 @@ for session in sessions:
 method_name = 'search'
 domain = [('name', '=', 'Curso Odoo 1')]
 course_ids = call('openacademy.course', method_name, domain)
-#course_id = course_ids[0]
+course_id = course_ids[0]
 print "course_ids",course_ids
 
 
-method_name = 'create'
-course_id = call('openacademy.course', method_name, {'name': 'Curso Odoo 1'})
+#method_name = 'create'
+#course_id = call('openacademy.course', method_name, {'name': 'Curso Odoo 1'})
+
+responsible_id = call('res.partner', 'search', [('name', '=', 'Vauxoo')])[0]
+print "responsible_id", responsible_id
 
 method_name = 'create'
 new_sesion_id = call(model, method_name, {
        'name': 'Sesion from ws', 
+       'instructor_id': responsible_id,
        'course_id': course_id,
+       'attendee_ids': [(4, 7), (4, 3)],
  
      })
 print "new_sesion_id",new_sesion_id
