f1dc72ebfd7ede23f923d93df2e0ff87ca053b80
Papi Chalo <gmlp_24a@hotmail.com>
2016-01-06 05:48:13 +0000

    [REF][website] Add teacher biography route

--- openacademy/controllers/main.py
+++ openacademy/controllers/main.py
@@ -10,3 +10,8 @@ class Academy(Controller):
         return http.request.render('openacademy.index', {
             'teachers': teachers.search([])
         })
+    @http.route('/academy/<model("openacademy.teacher"):teacher>/', auth='public', website=True)
+    def teacher(self, teacher):
+        return http.request.render('openacademy.biography', {
+            'person': teacher
+        })
